93da95d061dc3201e84840b60f61b54eb302bae4
Joseph Lee <joseph@jc-lab.net>
2024-02-20 18:14:00 +0900

    feat: abstract-wsman-netty

--- abstract-wsman-netty/build.gradle.kts
+++ abstract-wsman-netty/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     `signing`
 
     kotlin("jvm") version Version.KOTLIN
-    id("io.mateo.cxf-codegen") version "2.2.0" // "1.2.1"
 }
 
 repositories {
@@ -19,11 +18,6 @@ java {
 }
 
 dependencies {
-    cxfCodegen(platform("org.apache.cxf:cxf-bom:4.0.3"))
-    cxfCodegen("jakarta.xml.ws:jakarta.xml.ws-api:4.0.1")
-    cxfCodegen("jakarta.annotation:jakarta.annotation-api:2.1.1")
-    cxfCodegen("org.apache.cxf:cxf-xjc-plugin:4.0.0")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     testImplementation("org.apache.httpcomponents.client5:httpclient5:5.3")
@@ -38,39 +32,9 @@ dependencies {
     api(project(":abstract-wsman"))
 }
 
-cxfCodegen {
-    cxfVersion.set("4.0.3")
-}
-
-val wsdlListFile = file("$buildDir/wsdlList.tmp")
-tasks.register("generateWsdl", io.mateo.cxf.codegen.wsdl2java.Wsdl2Java::class) {
-    doFirst {
-        wsdlListFile.writeText(
-            listOf(
-                file("$projectDir/src/main/resources/xsd/enumeration.wsdl"),
-                file("$projectDir/src/main/resources/xsd/wsman.wsdl"),
-                file("$projectDir/src/main/resources/xsd/transfer.wsdl"),
-            )
-                .map { it.absolutePath }
-                .joinToString("\n")
-        )
-    }
-    outputs.file(wsdlListFile)
-    toolOptions {
-        wsdl.set(wsdlListFile.absolutePath)
-        wsdlList.set(true)
-        bindingFiles.set(listOf("$projectDir/src/main/resources/global.xjb"))
-        catalog.set("$projectDir/src/main/resources/wsdl2java-catalog.txt")
-        markGenerated.set(true)
-        verbose.set(true)
-        extendedSoapHeaders.set(true)
-    }
-}
-
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
-
 
 publishing {
     publications {
